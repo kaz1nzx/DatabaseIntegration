dfd18c20426c197ceaff41693e822d9ee72c89fd
kauazin <kauaadias2008@gmail.com>
2026-02-10 11:12:47 -0300

    Implement Endereço entity with fields and constructors
    Added fields and constructors for address entity.

--- petShop/src/main/java/com/petShop/Entity/Endereço.java
+++ petShop/src/main/java/com/petShop/Entity/Endereço.java
@@ -1,5 +1,43 @@
 package com.petShop.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Endereço {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String rua;
+	private int numero;
+	private String bairro;
+	private String cidade;
+	
+	public Endereço() {
+		
+	}
+	public Endereço (String rua, int numero, String bairro, String cidade) {
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		
+		
+	}
+	public String getRua() {
+		return rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
 
 }
